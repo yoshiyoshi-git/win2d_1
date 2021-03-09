b7f9699a0d653f743da54f8294c3d3a583ea88fc
lilac <github@nijisanji.sakura.ne.jp>
2021-03-09 22:30:27 +0900

    プロジェクト ファイルを追加します。

---
+++ win2d_1/MainPage.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::win2d_1::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
+void winrt::win2d_1::implementation::MainPage::CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args)
+{
+    args.DrawingSession().DrawText(L"Hello, world!", 250, 280, winrt::Windows::UI::Colors::Black());
+    args.DrawingSession().DrawCircle(300, 300, 200, winrt::Windows::UI::Colors::BlueViolet(), 5);
+    args.DrawingSession().FillRoundedRectangle(600, 50, 300, 200, 20, 20, winrt::Windows::UI::Colors::MediumSeaGreen());
+    args.DrawingSession().DrawText(L"Hello, world!", 700, 125, winrt::Windows::UI::Colors::Aqua());
+}
---
+++ win2d_1/MainPage.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+#include <Microsoft.Graphics.Canvas.UI.Xaml.h>
+
+namespace winrt::win2d_1::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+    };
+}
+
+namespace winrt::win2d_1::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
